2353ed3877985f45f90f2c667bb8f802969a4c1e
advokat977 <advokat@ugovor24.com>
2025-08-13 15:06:48 +0200

    Update submit-ugovor-o-djelu.js

--- api/submit-ugovor-o-djelu.js
+++ api/submit-ugovor-o-djelu.js
@@ -5,7 +5,7 @@ import { GoogleGenerativeAI } from '@google/generative-ai';
 import { Resend } from 'resend';
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 
-// Supabase URL i anonimni ključ se uzimaju iz Vercel varijabli okruženja
+// Supabase URL i anonimni kljuc se uzimaju iz Vercel varijabli okruzenja
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
@@ -20,72 +20,72 @@ const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 // Master templejti za ugovore (kao JS stringovi)
 const masterTemplates = {
     'Ugovor o djelu': `UGOVOR O DJELU
-Zaključen u {{mjesto_zakljucenja}}, dana {{datum_zakljucenja}} godine, između:
-1.  **Naručioca posla:** {{naziv_narucioca}}, sa sjedištem/prebivalištem na adresi {{adresa_narucioca}}, JMBG/PIB: {{id_broj_narucioca}} (u daljem tekstu: Naručilac), i
-2.  **Izvršioca posla:** {{naziv_izvrsioca}}, sa sjedištem/prebivalištem na adresi {{adresa_izvrsioca}}, JMBG/PIB: {{id_broj_izvrsioca}} (u daljem tekstu: Izvršilac).
+Zakljucen u {{mjesto_zakljucenja}}, dana {{datum_zakljucenja}} godine, izmedu:
+1.  **Narucioca posla:** {{naziv_narucioca}}, sa sjedistem/prebivalistem na adresi {{adresa_narucioca}}, JMBG/PIB: {{id_broj_narucioca}} (u daljem tekstu: Narucilac), i
+2.  **Izvrsioca posla:** {{naziv_izvrsioca}}, sa sjedistem/prebivalistem na adresi {{adresa_izvrsioca}}, JMBG/PIB: {{id_broj_izvrsioca}} (u daljem tekstu: Izvršilac).
 
 PREAMBULA
-Imajući u vidu zajedničke poslovne interese, ugovorne strane ovim Ugovorom uspostavljaju ugovorni odnos povodom angažovanja Izvršioca na poslovima opisanim u članu 1. ovog Ugovora, te regulišu prava i obaveze koje po tom osnovu proizilaze.
+Imajuci u vidu zajednicke poslovne interese, ugovorne strane ovim Ugovorom uspostavljaju ugovorni odnos povodom angazovanja Izvrsioca na poslovima opisanim u clanu 1. ovog Ugovora, te regulisu prava i obaveze koje po tom osnovu proizilaze.
 
-Član 1: Predmet Ugovora
-Izvršilac se obavezuje da za Naručioca, po njegovim uputstvima i zahtjevima, izvrši sljedeći posao: {{predmet_ugovora}} (u daljem tekstu: Djelo).
+Clan 1: Predmet Ugovora
+Izvršilac se obavezuje da za Narucioca, po njegovim uputstvima i zahtjevima, izvrsi sljedeći posao: {{predmet_ugovora}} (u daljem tekstu: Djelo).
 
-Član 2: Rok i Način Isporuke
-Izvršilac se obavezuje da posao iz člana 1. ovog Ugovora završi u potpunosti i preda Naručiocu najkasnije do {{rok_zavrsetka}}.
+Clan 2: Rok i Nacin Isporuke
+Izvršilac se obavezuje da posao iz clana 1. ovog Ugovora zavrsi u potpunosti i preda Naruciocu najkasnije do {{rok_zavrsetka}}.
 {{#if isporuka_definisana}}
 Pod isporukom se podrazumijeva {{definicija_isporuke}}.
 {{/if}}
 
-Član 3: Faze Rada, Revizije i Prihvatanje Djela
+Clan 3: Faze Rada, Revizije i Prihvatanje Djela
 {{#if definisan_proces_revizije}}
-1. Izvršilac će Naručiocu predstaviti početni predlog Djela.
-2. Naručilac ima pravo na ukupno {{broj_revizija}} kruga revizija (ispravki) na predlog Djela, koji su uključeni u ugovorenu cijenu. Svaka revizija podrazumijeva set objedinjenih komentara i zahtjeva za izmjenama.
-3. Naručilac je dužan da svoje eventualne zahtjeve za izmjenama dostavi Izvršiocu u roku od {{rok_za_feedback}} radna dana od dana prijema predloga. Ukoliko Naručilac u navedenom roku ne dostavi zahtjeve za izmjenama niti obavijesti Izvršioca o prihvatanju predloga, smatraće se da je predlog prećutno prihvaćen.
-4. Nakon što Naručilac pisanim putem (putem elektronske pošte) potvrdi da prihvata finalnu verziju Djela, smatra se da je Djelo primljeno i prihvaćeno.
+1. Izvršilac ce Naruciocu predstaviti pocetni predlog Djela.
+2. Narucilac ima pravo na ukupno {{broj_revizija}} kruga revizija (ispravki) na predlog Djela, koji su ukljuceni u ugovorenu cijenu. Svaka revizija podrazumijeva set objedinjenih komentara i zahtjeva za izmjenama.
+3. Narucilac je duzan da svoje eventualne zahtjeve za izmjenama dostavi Izvrsiocu u roku od {{rok_za_feedback}} radna dana od dana prijema predloga. Ukoliko Narucilac u navedenom roku ne dostavi zahtjeve za izmjenama niti obavijesti Izvrsioca o prihvatanju predloga, smatrace se da je predlog precutno prihvacen.
+4. Nakon sto Narucilac pisanim putem (putem elektronske poste) potvrdi da prihvata finalnu verziju Djela, smatra se da je Djelo primljeno i prihvaceno.
 {{/if}}
 
-Član 4: Naknada
-Naručilac se obavezuje da Izvršiocu za uredno izvršen i isporučen posao isplati naknadu u ukupnom {{tip_naknade}} iznosu od {{iznos_naknade_broj}} € (slovima: {{iznos_naknade_slovima}} eura).
-Isplata će se izvršiti u roku od {{rok_placanja}} dana od dana konačnog prihvatanja Djela u skladu sa članom 3. ovog Ugovora, uplatom na žiro račun Izvršioca broj: {{racun_izvrsioca}} koji se vodi kod {{banka_izvrsioca}}.
+Clan 4: Naknada
+Narucilac se obavezuje da Izvrsiocu za uredno izvrsen i isporucen posao isplati naknadu u ukupnom {{tip_naknade}} iznosu od {{iznos_naknade_broj}} € (slovima: {{iznos_naknade_slovima}} eura).
+Isplata ce se izvrsiti u roku od {{rok_placanja}} dana od dana konacnog prihvatanja Djela u skladu sa clanom 3. ovog Ugovora, uplatom na ziro racun Izvrsioca broj: {{racun_izvrsioca}} koji se vodi kod {{banka_izvrsioca}}.
 
-Član 5: Autorska Prava
+Clan 5: Autorska Prava
 {{#if je_autorsko_djelo}}
-1. Izvršilac potvrđuje da je Djelo koje je predmet ovog Ugovora njegovo originalno autorsko djelo.
-2. Ugovorenom naknadom iz člana 4. ovog Ugovora, Izvršilac isključivo, trajno i bez prostornog i vremenskog ograničenja prenosi na Naručioca sva imovinska autorska prava na Djelu.
-3. Prenos prava iz stava 2. ovog člana obuhvata, ali se ne ograničava na: pravo na umnožavanje, pravo na distribuciju, pravo na javno saopštavanje, pravo na adaptaciju, preradu i druga preoblikovanja Djela, kao i pravo na emitovanje i reemitovanje.
-4. Naručilac stiče pravo da Djelo registruje kao žig i da ga koristi u komercijalne i nekomercijalne svrhe bez ikakvih daljih saglasnosti ili naknada Izvršiocu.
-5. Izvršilac zadržava svoja moralna autorska prava u skladu sa zakonom, i saglasan je da Naručilac nije u obavezi da prilikom svake upotrebe Djela navodi ime Izvršioca.
+1. Izvršilac potvrduje da je Djelo koje je predmet ovog Ugovora njegovo originalno autorsko djelo.
+2. Ugovorenom naknadom iz clana 4. ovog Ugovora, Izvršilac iskljucivo, trajno i bez prostornog i vremenskog ogranicenja prenosi na Narucilac sva imovinska autorska prava na Djelu.
+3. Prenos prava iz stava 2. ovog clana obuhvata, ali se ne ogranicava na: pravo na umnozavanje, pravo na distribuciju, pravo na javno saopštavanje, pravo na adaptaciju, preradu i druga preoblikovanja Djela, kao i pravo na emitovanje i reemitovanje.
+4. Narucilac stice pravo da Djelo registruje kao zig i da ga koristi u komercijalne i nekomercijalne svrhe bez ikakvih daljih saglasnosti ili naknada Izvrsiocu.
+5. Izvršilac zadrzava svoja moralna autorska prava u skladu sa zakonom, i saglasan je da Narucilac nije u obavezi da prilikom svake upotrebe Djela navodi ime Izvrsioca.
 {{/if}}
 
-Član 6: Garancija Originalnosti i Zaštita od Pravnih Nedostataka
+Clan 6: Garancija Originalnosti i Zastita od Pravnih Nedostataka
 {{#if je_autorsko_djelo}}
-Izvršilac garantuje Naručiocu da je Djelo u cjelosti njegova originalna tvorevina, da nije opterećeno pravima trećih lica, te da njegovom upotrebom Naručilac ne povređuje bilo kakva autorska ili druga prava trećih lica.
+Izvršilac garantuje Narucilac da je Djelo u cjelosti njegova originalna tvorevina, da nije optereceno pravima trecih lica, te da njegovom upotrebom Narucilac ne povreduje bilo kakva autorska ili druga prava trecih lica.
 {{/if}}
 
-Član 7: Povjerljivost i Zaštita Podataka
+Clan 7: Povjerljivost i Zastita Podataka
 {{#if pristup_povjerljivim_info}}
-Izvršilac se obavezuje da će sve informacije, podatke i materijale do kojih dođe tokom rada na izradi Djela, a koji se odnose na poslovanje Naručioca, tretirati kao strogu poslovnu tajnu.
+Izvršilac se obavezuje da ce sve informacije, podatke i materijale do kojih dode tokom rada na izradi Djela, a koji se odnose na poslovanje Narucilac, tretirati kao strogu poslovnu tajnu.
 {{/if}}
 
-Član 8: Samostalnost Izvršioca
-Izvršilac je samostalan u izvršavanju posla i nije u radnom odnosu kod Naručioca.
+Clan 8: Samostalnost Izvršilaca
+Izvršilac je samostalan u izvršavanju posla i nije u radnom odnosu kod Narucilaca.
 
-Član 9: Cjelovitost Ugovora (Integralna Volja)
-Odredbe ovog Ugovora predstavljaju cjelokupnu volju ugovornih strana i sadrže sve o čemu su se ugovarači sporazumjeli.
+Clan 9: Cjelovitost Ugovora (Integralna Volja)
+Odredbe ovog Ugovora predstavljaju cjelokupnu volju ugovornih strana i sadrze sve o cemu su se ugovoraci sporazumjeli.
 
-Član 10: Izmjene i Dopune
-Izmjene i dopune ovog Ugovora mogu se vršiti isključivo u pisanoj formi, u vidu aneksa koji potpišu obje ugovorne strane.
+Clan 10: Izmjene i Dopune
+Izmjene i dopune ovog Ugovora mogu se vrsiti iskljucivo u pisanoj formi, u vidu aneksa koji potpisu obje ugovorne strane.
 
-Član 11: Rješavanje Sporova
-Ugovorne strane su saglasne da sve eventualne sporove koji proisteknu iz ovog Ugovora rješavaju sporazumno.
-Ukoliko to ne bude moguće, prihvataju nadležnost Osnovnog suda u {{mjesto_suda}}.
+Clan 11: Rjesavanje Sporova
+Ugovorne strane su saglasne da sve eventualne sporove koji proisteknu iz ovog Ugovora rjesavaju sporazumno.
+Ukoliko to ne bude moguce, prihvataju nadleznost Osnovnog suda u {{mjesto_suda}}.
 
-Član 12: Završne Odredbe
-Na sve odnose ugovornih strana koji nisu obuhvaćeni ovim Ugovorom primjenjivaće se odredbe Zakona o obligacionim odnosima i Zakona o autorskom i srodnim pravima Crne Gore.
-Ugovor je sačinjen u 2 (dva) istovjetna primjerka, po jedan za svaku ugovornu stranu.
+Clan 12: Zavrsne Odredbe
+Na sve odnose ugovornih strana koji nisu obuhvaceni ovim Ugovorom primjenjivace se odredbe Zakona o obligacionim odnosima i Zakona o autorskom i srodnim pravima Crne Gore.
+Ugovor je sacinjen u 2 (dva) istovjetna primjerka, po jedan za svaku ugovornu stranu.
 <br>
-**NARUČILAC POSLA** _________________________ {{naziv_narucioca}}
-**IZVRŠILAC POSLA** _________________________ {{naziv_izvrsioca}}`,
+**NARUCILAC POSLA** _________________________ {{naziv_narucioca}}
+**IZVRSILAC POSLA** _________________________ {{naziv_izvrsioca}}`,
 };
 
 async function generateInvoicePDF(orderId, ugovorType, totalPrice) {
@@ -96,12 +96,11 @@ async function generateInvoicePDF(orderId, ugovorType, totalPrice) {
     const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
     const primaryColor = rgb(0, 0.49, 1); // Plava boja za isticanje
 
-    // URL tvog sajta za logo, prilagođeno da radi i na lokalnoj mašini za testiranje
+    // URL tvog sajta za logo
     const logoUrl = 'https://ugovor24-site.vercel.app/logo.png';
     const logoBytes = await fetch(logoUrl).then(res => res.arrayBuffer());
     const logoImage = await pdfDoc.embedPng(logoBytes);
     
-    // Dimenzije loga
     const logoDims = logoImage.scale(0.5);
 
     page.drawImage(logoImage, {
@@ -115,16 +114,16 @@ async function generateInvoicePDF(orderId, ugovorType, totalPrice) {
     page.drawText('PREDRACUN', { x: 50, y: 310, size: 24, font: font, color: primaryColor });
     page.drawText('____________________________________________', { x: 50, y: 305, size: 12, font: font, color: primaryColor });
     
-    page.drawText(`Broj narudžbine: ${orderId}`, { x: 50, y: 280, size: 12, font: font });
+    page.drawText(`Broj narudzine: ${orderId}`, { x: 50, y: 280, size: 12, font: font });
     page.drawText(`Usluga: ${ugovorType}`, { x: 50, y: 260, size: 12, font: font });
-    page.drawText(`Iznos za uplatu: ${totalPrice} €`, { x: 50, y: 240, size: 12, font: font, color: primaryColor });
+    page.drawText(`Iznos za uplatu: ${totalPrice} EUR`, { x: 50, y: 240, size: 12, font: font, color: primaryColor });
     
-    // Podaci o plaćanju
-    page.drawText('Instrukcije za plaćanje:', { x: 50, y: 200, size: 14, font: font });
-    page.drawText('Primalac: Advokatska kancelarija Dejan Radinović', { x: 50, y: 180, size: 12, font: font });
-    page.drawText('Adresa: Božane Vučinić 7-5, 81000 Podgorica, Crna Gora', { x: 50, y: 165, size: 12, font: font });
+    // Podaci o placanju
+    page.drawText('Instrukcije za placanje:', { x: 50, y: 200, size: 14, font: font });
+    page.drawText('Primalac: Advokatska kancelarija Dejan Radinovic', { x: 50, y: 180, size: 12, font: font });
+    page.drawText('Adresa: Bozane Vucinica 7-5, 81000 Podgorica, Crna Gora', { x: 50, y: 165, size: 12, font: font });
     page.drawText('Banka: Erste bank AD Podgorica', { x: 50, y: 150, size: 12, font: font });
-    page.drawText('Broj računa: 540-0000000011285-46', { x: 50, y: 135, size: 12, font: font });
+    page.drawText('Broj racuna: 540-0000000011285-46', { x: 50, y: 135, size: 12, font: font });
     
     const pdfBytes = await pdfDoc.save();
     return pdfBytes;
@@ -139,12 +138,12 @@ async function sendConfirmationEmail(clientEmail, ugovorType, totalPrice, orderI
             to: clientEmail,
             subject: `Potvrda zahtjeva za ${ugovorType} - ugovor24.com`,
             html: `
-                <p>Poštovani/a,</p>
-                <p>Ovo je automatska potvrda da je Vaš zahtjev za ${ugovorType} uspješno primljen.</p>
-                <p>Nacrt Vašeg ugovora je već u procesu generisanja uz pomoć AI-a i biće spreman za pregled čim Vaša uplata bude proknjižena.</p>
-                <p>Molimo izvršite uplatu bankarskim transferom u iznosu od ${totalPrice} €. Predračun sa instrukcijama za plaćanje je u prilogu.</p>
-                <p>Nakon što uplata bude proknjižena na našem računu, ugovor će biti pregledan i poslat Vam u roku od 24 časa.</p>
-                <p>Srdačan pozdrav,</p>
+                <p>Postovani/a,</p>
+                <p>Ovo je automatska potvrda da je Vas zahtjev za ${ugovorType} uspjesno primljen.</p>
+                <p>Nacrt Vaseg ugovora je vec u procesu generisanja uz pomoc AI-a i bice spreman za pregled cim Vasa uplata bude proknjizena.</p>
+                <p>Molimo izvršite uplatu bankarskim transferom u iznosu od ${totalPrice} €. Predracun sa instrukcijama za placanje je u prilogu.</p>
+                <p>Nakon sto uplata bude proknjizena na nasem racunu, ugovor ce biti pregledan i poslat Vam u roku od 24 casa.</p>
+                <p>Srdacan pozdrav,</p>
                 <p>Tim ugovor24.com</p>
             `,
             attachments: [
@@ -156,7 +155,7 @@ async function sendConfirmationEmail(clientEmail, ugovorType, totalPrice, orderI
         });
         return { success: true };
     } catch (error) {
-        console.error('Greška pri slanju e-maila:', error);
+        console.error('Greska pri slanju e-maila:', error);
         return { error: 'Failed to send email' };
     }
 }
@@ -169,7 +168,7 @@ async function generateContractDraft(orderId, ugovorType, contractData) {
     }
     
     // Priprema prompta za Gemini AI
-    const prompt = `Ti si stručni advokat iz Crne Gore. Na osnovu priloženih podataka i master templejta, generiši nacrt ugovora.
+    const prompt = `Ti si strucni advokat iz Crne Gore. Na osnovu prilozenih podataka i master templejta, generisi nacrt ugovora.
     
     Pravni kontekst:
     - Pravo Crne Gore
@@ -181,19 +180,19 @@ async function generateContractDraft(orderId, ugovorType, contractData) {
     Master template:
     ${template}
     
-    Molim te, vrati mi samo konačan tekst ugovora, sa popunjenim podacima i uklonjenim placeholderima poput {{#if...}}, u formatu pogodnom za kopiranje i finalizaciju. Ne dodaj nikakav uvodni ili zaključni tekst, samo čisti tekst ugovora.`;
+    Molim te, vrati mi samo konacan tekst ugovora, sa popunjenim podacima i uklonjenim placeholderima poput {{#if...}}, u formatu pogodnom za kopiranje i finalizaciju. Ne dodaj nikakav uvodni ili zakljucni tekst, samo cisti tekst ugovora.`;
 
     const result = await model.generateContent(prompt);
     const generatedDraft = result.response.text();
     
-    // Ažuriranje narudžbine u bazi sa generisanim nacrtom
+    // Azuriranje narudzine u bazi sa generisanim nacrtom
     const { error: updateError } = await supabase
         .from('orders')
         .update({ generated_draft: generatedDraft, is_draft_generated: true })
         .eq('id', orderId);
 
     if (updateError) {
-        console.error('Greška pri ažuriranju narudžbine:', updateError);
+        console.error('Greska pri azuriranju narudzine:', updateError);
         return { error: 'Database update error' };
     }
     
@@ -229,13 +228,13 @@ export default async function handler(req, res) {
       .select();
 
     if (orderError) {
-      console.error('Greška pri unosu u orders tabelu:', orderError);
+      console.error('Greska pri unosu u orders tabelu:', orderError);
       return res.status(500).json({ error: 'Database insertion error' });
     }
     
     const order_id = orderData[0].id;
     
-    // 2. Unos specifičnih podataka u 'orders_ugovor_o_djelu' tabelu
+    // 2. Unos specificnih podataka u 'orders_ugovor_o_djelu' tabelu
     const { error: specificError } = await supabase
       .from('orders_ugovor_o_djelu')
       .insert([
@@ -267,22 +266,22 @@ export default async function handler(req, res) {
       ]);
 
     if (specificError) {
-      console.error('Greška pri unosu u orders_ugovor_o_djelu tabelu:', specificError);
+      console.error('Greska pri unosu u orders_ugovor_o_djelu tabelu:', specificError);
       return res.status(500).json({ error: 'Database insertion error' });
     }
 
     // 3. Pokretanje AI generisanja (automatski) i slanje e-maila sa predracunom
     const generationResult = await generateContractDraft(order_id, ugovor_type, formData);
     if (generationResult.error) {
-        console.error('Greška pri generisanju nacrta:', generationResult.error);
+        console.error('Greska pri generisanju nacrta:', generationResult.error);
     }
     
     const emailResult = await sendConfirmationEmail(client_email, ugovor_type, total_price, order_id);
     if (emailResult.error) {
-        console.error('Greška pri slanju e-maila:', emailResult.error);
+        console.error('Greska pri slanju e-maila:', emailResult.error);
     }
     
-    // 4. Preusmjeravanje klijenta na stranicu za plaćanje
+    // 4. Preusmjeravanje klijenta na stranicu za placanje
     res.writeHead(302, {
       'Location': `/placanje.html?cijena=${total_price}&ugovor=${encodeURIComponent(ugovor_type)}`,
       'Content-Type': 'text/plain',
@@ -290,7 +289,7 @@ export default async function handler(req, res) {
     res.end('Redirecting to payment...');
     
   } catch (err) {
-    console.error('Neočekivana greška:', err);
+    console.error('Neoocekivana greska:', err);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 }
